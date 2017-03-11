079c140d65c1547227654cfbefca3be90dd6e8b5
adortega90 <adortega90@gmail.com>
2017-03-11 16:25:52 -0500

    Add files via upload

--- prueba/js/custom.js
+++ prueba/js/custom.js
@@ -1,40 +1,13 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-// preloader
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
-});
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
 
-/* HTML document is loaded. DOM is ready. 
--------------------------------------------*/
-$(function(){
-
-  // ------- WOW ANIMATED ------ //
-  wow = new WOW(
-  {
-    mobile: false
-  });
-  wow.init();
-
-  // ------- JQUERY PARALLAX ---- //
-  function initParallax() {
-    $('#home').parallax("100%", 0.1);
-    $('#gallery').parallax("100%", 0.3);
-    $('#menu').parallax("100%", 0.2);
-    $('#team').parallax("100%", 0.3);
-    $('#contact').parallax("100%", 0.1);
-
-  }
-  initParallax();
-
-  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
-  $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
-  // NIVO LIGHTBOX
-  $('#gallery a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-
-});
-
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
